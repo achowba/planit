ee59f178ec0713d2c19e905f75809550d1271c69
achowba <achowba>
2019-10-27 01:06:57 +0400

    change server config

--- src/server/index.js
+++ src/server/index.js
@@ -3,9 +3,14 @@ const http = require('http');
 
 const app = require('./app');
 
-const port = process.env.PORT || 2806;
+/* const port = process.env.PORT || 4000;
 const server = http.createServer(app);
 
 server.listen((port), () => {
 	console.log(`App running on Port: ${port}`);
+}); */
+
+app.set( 'port', (process.env.PORT || 4000));
+app.listen( app.get( 'port' ), function() {
+	console.log( `Node server is running on Port: ${app.get('port')}`);
 });
