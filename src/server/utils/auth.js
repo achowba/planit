bac74dc5ad675eb83ffc1b9b9cba362170e83874
Prince <justprince9@gmail.com>
2020-09-14 18:50:32 +0400

    replace bcrypt with bcryptjs

--- src/server/utils/auth.js
+++ src/server/utils/auth.js
@@ -1,4 +1,5 @@
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 async function hashPassword (password) {
